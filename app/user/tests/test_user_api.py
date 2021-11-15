faab6d0fa15ddef30049160ad1e52ad52d0ade48
SatanJordon <sayan7chakraborty@gmail.com>
2021-11-15 23:13:55 +0530

    updated user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,13 +6,14 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 class PublicUserApiTests(TestCase):
     def setUp(self):
-        self.cleint = APIClient()
+        self.client = APIClient()
 
     def test_create_valid_user_success(self):
         payload = {
@@ -20,7 +21,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass',
             'name': 'Test Name'
         }
-        res = self.cleint.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
@@ -33,7 +34,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass'
         }
          create_user(**payload)
-         res = self.cleint.post(CREATE_USER_URL, payload)
+         res = self.client.post(CREATE_USER_URL, payload)
 
          self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -43,8 +44,44 @@ class PublicUserApiTests(TestCase):
             'password': 'pw',
             'name': 'Test Name'
         }
-        res = self.cleint.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+         payload = {
+            'email': 'test@kolkata.com',
+            'password': 'testpass',
+            'name': 'Test Name'
+        }
+         create_user(**payload)
+         res = self.client.post(TOKEN_URL, payload)
+
+         self.assertIn('token', res.data)
+         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credential(self):
+         create_user(email='test@kolkata.com', password='testpass')
+         payload = {
+             'email': 'test@kolkata.com',
+             'password': 'wrong'
+         }
+         res = self.client.post(TOKEN_URL,payload)
+         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+         self.assertNotIn('token', res.data)
+
+    def test_create_token_no_user(self):
+         payload = {
+             'email': 'test@kolkata.com',
+             'password': 'testpass'
+         }
+         res = self.client.post(TOKEN_URL,payload)
+         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+         self.assertNotIn('token', res.data)
+
+    def test_create_token_missing_fields(self):
+         res = self.client.post(TOKEN_URL,{'email':'one', 'password': ''})
+         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+         self.assertNotIn('token', res.data)
